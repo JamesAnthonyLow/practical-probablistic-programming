9112783e54342a2127d9b2f8ab2d32452e69f03a
JamesAnthonyLow <JamesALowenthal@gmail.com>
2016-07-02 15:55:56 +0000

    add HelloWorld example

---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,36 @@
+import com.cra.figaro.language.{Flip, Select}
+import com.cra.figaro.library.compound.If
+import com.cra.figaro.algorithm.factored.VariableElimination
+
+object HelloWorld {
+  val sunnyToday = Flip(0.2)
+  val greetingToday = If(sunnyToday,
+    Select(0.6 -> "Hello, world!", 0.4 -> "Howdy, universe!"),
+    Select(0.2 -> "Hello, world!", 0.8 -> "Oh no, not again"))
+  val sunnyTommorrow = If(sunnyToday, Flip(0.8), Flip(0.05))
+  val greetingTommorrow = If(sunnyTommorrow,
+    Select(0.6 -> "Hello, world!", 0.4 -> "Howdy, universe!"),
+    Select(0.2 -> "Hello, world!", 0.8 -> "Oh no, not again"))
+  def predict() {
+    val result = VariableElimination.probability(greetingToday, "Hello, world!")
+    println("Today's greeting is \"Hello world!\" " + "with probability " + result + ".")
+  }
+  def infer() {
+    greetingToday.observe("Hello, world!")
+    val result = VariableElimination.probability(sunnyToday, true)
+    println("If today's gretting is \"Hello, world!\", today's " + 
+      "weather is sunny with probability " + result + ".")
+  }
+  def learnAndPredict() {
+    greetingToday.observe("Hello, world!")
+    val result = VariableElimination.probability(greetingTommorrow, "Hello, world!")
+    println("If today's greeting is \"Hello, world!\", " + 
+      "tomorrow's greeting will be \"Hello, world!\" " + 
+      "with probability " + result + ".")
+  }
+  def main(args: Array[String]) {
+    predict()
+    infer()
+    learnAndPredict()
+  }
+}
