c350503afc55b675be5a4eb56df542ca2116cd4d
JamesAnthonyLow <JamesALowenthal@gmail.com>
2016-07-03 16:41:03 +0000

    learnAndPredictWeather

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,13 +1,14 @@
-import com.cra.figaro.language.{Flip, Select}
+import com.cra.figaro.language.{Flip, Select, Chain, Apply}
 import com.cra.figaro.library.compound.If
 import com.cra.figaro.algorithm.factored.VariableElimination
 
 object HelloWorld {
   val sunnyToday = Flip(0.2)
+  val sunnyTommorrow = If(sunnyToday, Flip(0.8), Flip(0.05))
+  val rightSideOfBed = Flip(0.6)
   val greetingToday = If(sunnyToday,
     Select(0.6 -> "Hello, world!", 0.4 -> "Howdy, universe!"),
     Select(0.2 -> "Hello, world!", 0.8 -> "Oh no, not again"))
-  val sunnyTommorrow = If(sunnyToday, Flip(0.8), Flip(0.05))
   val greetingTommorrow = If(sunnyTommorrow,
     Select(0.6 -> "Hello, world!", 0.4 -> "Howdy, universe!"),
     Select(0.2 -> "Hello, world!", 0.8 -> "Oh no, not again"))
@@ -21,16 +22,23 @@ object HelloWorld {
     println("If today's gretting is \"Hello, world!\", today's " + 
       "weather is sunny with probability " + result + ".")
   }
-  def learnAndPredict() {
-    greetingToday.observe("Hello, world!")
-    val result = VariableElimination.probability(greetingTommorrow, "Hello, world!")
-    println("If today's greeting is \"Hello, world!\", " + 
-      "tomorrow's greeting will be \"Hello, world!\" " + 
-      "with probability " + result + ".")
+  def learnAndPredict(greeting: String) {
+    greetingToday.observe(greeting)
+    val result = VariableElimination.probability(greetingTommorrow, greeting)
+    println(s"If today's greeting is $greeting, " +
+      s"tommorow's greeting will be $greeting with probability $result")
+  }
+  def learnAndPredictTodaysWeather(greeting: String) {
+    greetingToday.observe(greeting)
+    val result = VariableElimination.probability(sunnyToday, true)
+    println(s"If today's greeting is $greeting, "+
+      s"the weather will be sunny with a probability of $result")
   }
   def main(args: Array[String]) {
     predict()
     infer()
-    learnAndPredict()
+    learnAndPredict("Hello, world!")
+    learnAndPredict("Oh no, not again")
+    learnAndPredictTodaysWeather("Oh no, not again")
   }
 }
